bbb5c320a2eb792c3491f0607e2e7ce38140bc2d
MarkThielen <mark@markthielen.de>
2013-12-28 23:13:57 +0100

    - added Track Status data structure (lights status, fuel mode, pitlane flag, lap counter flag) - added in_pit and pits attributes to car status structure

--- carrera.h
+++ carrera.h
@@ -1,4 +1,9 @@
 
+// -------------------------------------------------------
+// - data structures describing the different responses
+// - received from the CU upon a "? command
+// -------------------------------------------------------
+
  struct STRUCT_CARRERA_LAPINFO {
     char timer[8];
     char group;
@@ -28,6 +33,28 @@
  
   };
 
+
+// -------------------------------------------------------------
+// - custom data structures
+// -------------------------------------------------------------
+
+struct STRUCT_CAR_STATUS {
+  unsigned int car_number;
+  unsigned int current_laptime;
+  unsigned int fastest_laptime;
+  unsigned int laps;
+  unsigned int fuel_status;
+  unsigned int active;
+  unsigned int in_pit;
+  unsigned int pits;
+};
+
+struct STRUCT_TRACK_STATUS {
+  unsigned int lights_status;
+  unsigned int fuel_mode;
+  unsigned int pitlane_installed;
+  unsigned int lapcounter_installed;
+};
 
 char CARRERA_COMMAND = 0x22;
 char CARRERA_LAPINFO = 0x3f;
