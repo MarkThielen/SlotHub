c7624493e35b82cedc270681125441efb330978f
MarkThielen <mark@markthielen.de>
2014-01-10 00:53:22 +0100

    - added cmd line parameter for bind address (ControlUnit) - added cmd line parameter for server address (Display) - ControlUnit and Display can no run in one process and external clients can connect as well

--- SlotHub.c
+++ SlotHub.c
@@ -43,14 +43,18 @@ int main (int argc, char **argv){
   struct arg_file *ttyDevice = arg_file0("d","device",NULL,"Serial Terminal");
   
   struct arg_lit *startControlUnit = arg_lit0("c","ControlUnit","start the ControlUnit");
-  struct arg_lit *startTextDisplay = arg_lit0("d","Display","start the Display");
+  struct arg_lit *startTextDisplay = arg_lit0("t","TimeDisplay","start the Display");
+  
+  struct arg_str *bindAddress = arg_str0("b","bind","protocol://address:port","address/port to bind (ControlUnit)");
+  struct arg_str *connectTo = arg_str0("s","server","protocol://address:port","address/port to connect to (TimeDisplay)");
+
 
   struct arg_end *end = arg_end(20);
 
   
 
   void *argtable[] = { 
-    startControlUnit, startTextDisplay, ttyDevice, end      
+    startControlUnit, startTextDisplay, ttyDevice,bindAddress, connectTo, end      
   };
 
   // check argtable
@@ -61,6 +65,14 @@ int main (int argc, char **argv){
   // will be overwritten if something was passed
   // on the cmd line
   ttyDevice->filename[0] = "/dev/ttyUSB0";
+  
+  // set default value for control unit 
+  // server to bind to
+  bindAddress->sval[0] = "tcp://*:8190";
+
+  // set default value for Time Display 
+  // client to connect to
+  connectTo->sval[0] = "tcp://localhost:8190";
 
 
   // parse argtable
@@ -69,30 +81,72 @@ int main (int argc, char **argv){
   if(parse_errors > 0)
     arg_print_errors(stdout,end,"SlotHub");
 
-  // Create TextDisplay Object and start it
-  
+  // --------------------------------------------
+  // - Create TextDisplay Object and start thread
+  // --------------------------------------------
   if (startTextDisplay->count > 0) {
-    TextDisplay *td = new TextDisplay();
+
+    TextDisplay *td = new TextDisplay(connectTo->sval[0]);
+
     std::thread tdt = td->start();
     std::cout << "TextDisplay started" << std::endl;
     tdt.join();
-    
+
+    arg_freetable(argtable,sizeof(argtable)/sizeof(argtable[0]));
+
+    return 0;
   }
 
-
-  if(startControlUnit->count > 0) {
-  // create a ControlUnit Object
-    ControlUnit *cu = new ControlUnit(ttyDevice->filename[0]);
+  // -------------------------------------------
+  // - create ControlUnit Object and start thread
+  // -------------------------------------------
+  else if(startControlUnit->count > 0) {
+   
+    
+    ControlUnit *cu = new ControlUnit(ttyDevice->filename[0],bindAddress->sval[0]);
   
     std::thread cut = cu->start();
-    
-    std::cout << "ControlUnit started" << std::endl;
-
+    std::cout << "ControlUnit started" << " (" << cu->getBindAddress()  << ")" << std::endl;
     cut.join();
 
-  }
+    arg_freetable(argtable,sizeof(argtable)/sizeof(argtable[0]));
+    
+    return 0;
 
- return 0;
+  }
+  // --------------------------------------------------------
+  // - start control unit and display in one process
+  // --------------------------------------------------------
+  else {
+
+    TextDisplay *td = new TextDisplay("ipc://SlotHub.ipc");
+    std::thread tdt = td->start();
+    
+    
+    ControlUnit *cu = new ControlUnit(ttyDevice->filename[0],bindAddress->sval[0]);
+    std::thread cut = cu->start();
+    
+    tdt.join();
+
+    // set execution flag to stop
+    cu->stop();
+
+    // wait for thread to terminate
+    cut.join();
+    
+
+    arg_freetable(argtable,sizeof(argtable)/sizeof(argtable[0]));    
+    return 0;
+  }
+  
+
+
+
+   arg_print_glossary(stdout,argtable," %-40s %s\n");
+
+   arg_freetable(argtable,sizeof(argtable)/sizeof(argtable[0]));
+
+   return 0;
 
 } 
 
